01bfc3da4e44b348ba222786f255e37417479fff
sunyidong <sunyidong@xmiles.cn>
2021-07-06 17:17:33 +0800

    在sunyidong分支提交第二次

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         System.out.println("在master分支提交第一次");
+        System.out.println("在master分支提交第二次");
         System.out.println("在sunyidong的分支提交一次");
     }
 }
