30741fb83d52cdc8ac6d7ace4b059377ed378ad1
sunyidong <sunyidong@xmiles.cn>
2021-07-06 18:01:11 +0800

    在master改动尝试reve

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("在sunyidong的分支提交一次");
         System.out.println("在sunyidong的分支提交2次");
         System.out.println("在sunyidong的分支提交2次");
-        System.out.println("在sunyidong改动尝试cheey-pick");
-        System.out.println("在sunyidong改动尝试cheey-pick2");
+        System.out.println("reve");
     }
 }
